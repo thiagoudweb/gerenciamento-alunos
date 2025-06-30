9c023593daf989c7bcf270b2942fd5635d86b633
Thiago Silva <thhzmonster@gmail.com>
2025-06-30 06:51:51 -0300

    UsuarioServiceTest: Implementando quatro funções para testes unitários da classe Usuario.
    Criação de quatro funções simples para testes unitários da feature Usuario

--- src/test/java/br/com/gerenciamento/service/UsuarioServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/UsuarioServiceTest.java
@@ -1,10 +1,83 @@
 package br.com.gerenciamento.service;
 
+import br.com.gerenciamento.exception.EmailExistsException;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import br.com.gerenciamento.model.Usuario;
+import br.com.gerenciamento.util.Util;
+import jakarta.transaction.Transactional;
+import jakarta.validation.ConstraintViolationException;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThrows;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 public class UsuarioServiceTest {
+
+    @Autowired
+    private ServiceUsuario serviceUsuario;
+
+    // #1 TESTE
+    @Test
+    public void testUsuarioDadosInvalidas() throws Exception {
+        // Given
+        Usuario usuario = new Usuario();
+        usuario.setEmail("thiagosilvs@gmail.com");
+        usuario.setUser("Thiago Silva");
+        usuario.setSenha("123");
+        this.serviceUsuario.salvarUsuario(usuario);
+
+        Usuario usuarioLogado = this.serviceUsuario.loginUser("Thiago Silva", Util.md5("senha"));
+
+
+        assertNull(usuarioLogado);
+    }
+    // #2 TESTE
+    @Test
+    public void testUsuarioEmailExiste() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setEmail("thiagosilvs@gmail.com");
+        usuario.setUser("Thiago Silva");
+        usuario.setSenha("123");
+        serviceUsuario.salvarUsuario(usuario);
+
+        Usuario usuario2 = new Usuario();
+        usuario.setEmail("thiagosilvs@gmail.com");
+        usuario.setUser("Thiago Silva");
+        usuario.setSenha("123");
+        Assert.assertThrows(EmailExistsException.class, () -> {
+            serviceUsuario.salvarUsuario(usuario2);});
+    }
+
+    // #3 TESTE
+    @Test
+    public void testSaveUsuarioInvalido() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setEmail("thiagosilvs@gmail.com");
+        usuario.setSenha("123");
+        usuario.setUser("Thiago");
+        assertThrows(ConstraintViolationException.class, () -> {
+            serviceUsuario.salvarUsuario(usuario);});
+
+    }
+
+    // #4 TESTE
+    @Test
+    public void testSalvarUsuario() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setEmail("thiagosilvs@gmail.com");
+        usuario.setSenha("123");
+        usuario.setUser("Thiago");
+        serviceUsuario.salvarUsuario(usuario);
+        Assert.assertNotNull("Usuario salvo", usuario.getId());
+    }
+
+
 }
